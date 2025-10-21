b59ed989d4867376b56fadcecc364f3b4ec19048
stephen-clem <stevebclem@gmail.com>
2025-10-21 00:19:32 -0500

    Adjust assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue' // or react, svelte, etc.
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/stephen-clem.github.io/' // replace with your repo name
+
 })
